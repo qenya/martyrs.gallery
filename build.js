94878d7379b1171f232b7759acf98d132694be54
Timon Walshe-Grey <me@timon.red>
2017-09-30 12:57:21 +0100

    Initial commit
    Little bit of a late switch to source control...

---
+++ build.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var Metalsmith  = require('metalsmith');
+var collections = require('metalsmith-collections');
+var layouts     = require('metalsmith-layouts');
+var paths       = require('metalsmith-paths');
+var permalinks  = require('metalsmith-permalinks');
+
+Metalsmith(__dirname)
+	.source('src')
+	.destination('build')
+	.use(permalinks({
+		relative: false
+	}))
+	.use(collections({
+		homepage: {
+			pattern: ["**/*.html", "!index.html"],
+			sortBy: "homepage-order"
+		},
+		"Plan a Visit": {
+			sortBy: "collection-order"
+		},
+		"About Us": {
+			sortBy: "collection-order"
+		},
+		"Get Involved": {
+			sortBy: "collection-order"
+		}
+	}))
+	.use(paths({
+		directoryIndex: "index.html"
+	}))
+	.use(require("metalsmith-debug")())
+	.use(layouts({
+		engine: 'nunjucks',
+		default: 'pages.html',
+		pattern: '**/*.html'
+	}))
+	.build(err => {if (err) throw err;});
